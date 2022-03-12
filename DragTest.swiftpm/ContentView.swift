3f2d3b234a6f1786e54019f78d106f938000b91c
Chris Clampitt <christian.clampitt@nyu.edu>
2022-03-11 20:25:12 -0700

    Fixed group downward movement case

--- DragTest.swiftpm/ContentView.swift
+++ DragTest.swiftpm/ContentView.swift
@@ -3,6 +3,101 @@ import SwiftUI
 let rectHeight: CGFloat = 100
 let rectWidth: CGFloat = 400
 
+
+struct RectItem: Identifiable, Equatable {
+    let id: Int
+    let color: Color
+    var location: CGPoint
+    var previousLocation: CGPoint
+    var children: [RectItem] = []
+    var zIndex: Int = 0
+    
+    // for converting items back into nested data
+    var parentId: Int? = nil
+    
+    init(id: Int, color: Color, location: CGPoint, children: [RectItem] = [], parentId: Int? = nil) {
+        self.id = id
+        self.color = color
+        self.location = location
+        self.previousLocation = location
+        self.children = children
+        self.parentId = parentId
+    }
+}
+
+extension Optional {
+    var isDefined: Bool {
+        self != nil
+    }
+}
+
+struct RectView2: View {
+    
+    var item: RectItem
+    @Binding var items: RectItems // all items
+    @Binding var current: Int?
+    
+    var body: some View {
+        Rectangle().fill(item.color)
+//            .border((current.map { $0 == item.id } ?? false) ? .gray : .clear,
+//                    width: 8)
+            .border((current.map { $0 == item.id } ?? false) ? .white : .clear,
+                    width: 16)
+         
+//            .overlay(Rect)
+            .frame(width: rectWidth, height: rectHeight)
+            .overlay(VStack {
+                Text("Id: \(item.id)")
+                Text("Parent?: \(item.parentId?.description ?? "None")")
+            }.scaleEffect(1.4)
+            )
+            .foregroundColor(.white)
+            .border(.orange)
+//            .position(item.location)
+//            .offset(CGSize(width: item.location.x,
+        
+            // while creating the items, can also create the indentation based not just on parentId.isDefined
+            // but also on how deep the level of nesting is
+//            .offset(CGSize(width: item.location.x + (item.parentId.isDefined ? 50 : 0),
+            .offset(CGSize(width: item.location.x,
+                           height: item.location.y))
+//            .zIndex(Double(item.zIndex))
+            .gesture(DragGesture()
+                        .onChanged({ value in
+                print("onChanged: \(item.id)")
+                // done in onDragged
+//                item.location = updatePosition(
+//                    translationHeight: value.translation.height,
+//                    location: item.previousLocation)
+                current = item.id
+                var item = item
+                item.zIndex = 9999
+                items = onDragged(item, // this dragged item
+                                  value.translation, // drag data
+                                  // ALL items
+                                  items)
+                
+                // ^^ now that we've updated the location of the item,
+                // we might need to also move the other items
+                
+                
+            })
+                        .onEnded({ _ in
+                print("onEnded: \(item.id)")
+                current = nil
+                var item = item
+                item.previousLocation = item.location
+                item.zIndex = 0 // set to zero when drag ended
+                let index = items.firstIndex { $0.id == item.id }!
+                items[index] = item
+                items = onDragEnded(item, items)
+                
+            })
+            ) // gesture
+    }
+}
+
+
 // works!
 func getMovedtoIndex(item: RectItem,
                      items: RectItems,
@@ -98,28 +193,96 @@ func updatePositionsHelper(_ item: RectItem,
 }
 
 
+func adjustMoveToIndex(calculatedIndex: Int,
+                       originalItemIndex: Int,
+                       movedIndices: [Int],
+                       maxIndex: Int) -> Int {
+    
+    var calculatedIndex = calculatedIndex
+    
+    // Suppose we have [blue, black, green],
+    // blue is black's parent,
+    // and blue and green are both top level.
+    // If we move blue down, `getMovedtoIndex` will give us a new index of 1 instead of 0.
+    // But index 1 is the position of blue's child!
+    // So we add the diff.
+    
+    
+//    if calculatedIndex > originalItemIndex {
+//        let diff = calculatedIndex - originalItemIndex
+//        print("diff: \(diff)")
+//        calculatedIndex = calculatedIndex + diff
+//        print("calculatedIndex is now: \(calculatedIndex)")
+//        if calculatedIndex > maxIndex {
+//            print("calculatedIndex was too large, will use max index instead")
+//            calculatedIndex = maxIndex
+//        }
+//        return calculatedIndex
+//    }
+    
+    if calculatedIndex > originalItemIndex {
+        let diff = calculatedIndex - originalItemIndex
+        print("diff: \(diff)")
+            
+        // movedIndices is never going to be empty!
+        // it always has at least a single item
+        if movedIndices.isEmpty {
+            calculatedIndex = calculatedIndex + diff
+            print("empty movedIndices: calculatedIndex is now: \(calculatedIndex)")
+        } else {
+            let maxMovedIndex = movedIndices.max()!
+            print("maxMovedIndex: \(maxMovedIndex)")
+            calculatedIndex = maxMovedIndex + diff
+            print("nonEmpty movedIndices: calculatedIndex is now: \(calculatedIndex)")
+        }
+        
+        if calculatedIndex > maxIndex {
+            print("calculatedIndex was too large, will use max index instead")
+            calculatedIndex = maxIndex
+        }
+        return calculatedIndex
+        
+    }
+    
+    else {
+        print("Will NOT adjust moveTo index")
+        return calculatedIndex
+    }
+}
+
+
 func maybeMoveIndices(_ items: RectItems,
                       indicesMoved: [Int],
                       to: Int,
                       originalIndex: Int) -> RectItems {
     
+    var items = items
+    
     if to != originalIndex {
         print("maybeMoveIndices: Will move...")
-        var items = items
-        
+                
         /*
          listData.move(fromOffsets: IndexSet(integer: from),
                        toOffset: to > from ? to + 1 : to)
          */
-        items.move(fromOffsets: IndexSet(indicesMoved),
-                   toOffset: to > originalIndex ? to + 1 : to)
         
-//        print("maybeMoveIndices: items after move: \(items)")
+//        print("maybeMoveIndices: items BEFORE move: \(items)")
+        print("maybeMoveIndices: items by color BEFORE move: \(items.map(\.color))")
+        
+        let finalOffset = to > originalIndex ? to + 1 : to
+        
+        print("maybeMoveIndices: finalOffset: \(finalOffset)")
+        
+        items.move(fromOffsets: IndexSet(indicesMoved),
+                   toOffset: finalOffset)
+        
+//        print("maybeMoveIndices: items AFTER move: \(items)")
+        print("maybeMoveIndices: items by color AFTER move: \(items.map(\.color))")
         
         items = setPositionsByIndices(items,
                                       isDragEnded: false)
         
-        // print("maybeMoveIndices: items after position reset by indices: \(items)")
+         print("maybeMoveIndices: items after position reset by indices: \(items)")
         
         return items
     } else {
@@ -142,29 +305,52 @@ func onDragged(_ item: RectItem, // assumes we've already
     print("onDragged: items was: \(items)")
     
     let originalItemIndex = items.firstIndex { $0.id == item.id }!
-        
+    
+
     // ASSUMES GROUP IS OPEN
     // doesn't matter if group is closed? Because if it is, then we won't have even added those items to our
-    let (newItems,
-         indicesofItemsToMove) = updatePositionsHelper(item,
+    let (newItems, newIndices) = updatePositionsHelper(item,
                                                        items,
                                                        [],
                                                        translation.height)
     
     items = newItems
     item = items[originalItemIndex] // update the `item` too!
+    var indicesofItemsToMove: [Int] = newIndices
+    
     
     print("onDragged: newItems: \(newItems)")
     print("onDragged: new item: \(item)")
     print("onDragged: indicesofItemsToMove: \(indicesofItemsToMove)")
     
-    // ^^ not just the item's immediate children, but ALL the children's children
-        
-    // eg [a, b, c] has max index of 2
-    let calculatedIndex = getMovedtoIndex(item: item, items: items)
+    
+    var calculatedIndex = getMovedtoIndex(item: item, items: items)
+    
+    calculatedIndex = adjustMoveToIndex(
+        calculatedIndex: calculatedIndex,
+        originalItemIndex: originalItemIndex,
+        movedIndices: indicesofItemsToMove,
+        maxIndex: items.count - 1)
 
-//    print("originalItemIndex: \(originalItemIndex)")
-//    print("calculatedIndex: \(calculatedIndex)")
+    
+//    // if we moved down, we need to adjust the
+//    if calculatedIndex > originalItemIndex {
+//        let diff = calculatedIndex - originalItemIndex
+//        print("diff: \(diff)")
+//        calculatedIndex = calculatedIndex + diff
+//        print("calculatedIndex is now: \(calculatedIndex)")
+//        if calculatedIndex > (items.count - 1) {
+//            print("calculatedIndex was too large, will use max index instead")
+//            calculatedIndex = items.count - 1
+//        }
+//    }
+    
+    // ^^ you don't want to change which items you're MOVING
+    // you just want to update the newly calculated position
+
+    
+    print("originalItemIndex: \(originalItemIndex)")
+    print("calculatedIndex: \(calculatedIndex)")
 
     return maybeMoveIndices(items,
                             indicesMoved: indicesofItemsToMove,
@@ -213,48 +399,6 @@ struct MyColor: Equatable {
 }
 
 
-let sampleColors0: [MyColor] = [
-    MyColor(color: .red),
-    MyColor(color: .blue),
-    MyColor(color: .green)
-]
-
-let sampleColors1: [MyColor] = [
-    MyColor(color: .red),
-    MyColor(color: .blue, children: [
-        MyColor(color: .black),
-        MyColor(color: .brown)
-    ]),
-    MyColor(color: .green)
-]
-
-let sampleColors2: [MyColor] = [
-    MyColor(color: .red),
-    MyColor(color: .blue, children: [
-        MyColor(color: .black),
-        MyColor(color: .brown, children: [
-            MyColor(color: .cyan),
-            MyColor(color: .purple),
-        ])
-    ]),
-    MyColor(color: .green)
-]
-
-let sampleColors3: [MyColor] = [
-    MyColor(color: .red),
-    MyColor(color: .blue, children: [
-        MyColor(color: .black),
-        MyColor(color: .brown, children: [
-            MyColor(color: .cyan),
-            MyColor(color: .purple, children: [
-                MyColor(color: .orange),
-                MyColor(color: .gray),
-            ]),
-        ])
-    ]),
-    MyColor(color: .green)
-]
-
 
 // Given a nested, ordered data structure, returns a flattened data structure with positions based on nesting + order
 // for creating master list: RectItems with positions based on nesting etc.
@@ -290,7 +434,7 @@ func itemsFromColorHelper(_ color: MyColor,
                           nestingLevel: Int,
                           viewHeight: Int = 100) -> (Int, RectItems, Int) {
     
-    print("itemsFromColorHelper: color: \(color)")
+//    print("itemsFromColorHelper: color: \(color)")
     var currentHighestIndex = currentHighestIndex
     var items = RectItems()
     var nestingLevel = nestingLevel
@@ -318,9 +462,9 @@ func itemsFromColorHelper(_ color: MyColor,
             parentId: item.id,
             nestingLevel: nestingLevel)
         
-        print("itemsFromColorHelper: newIndex: \(newIndex)")
-        print("itemsFromColorHelper: newItems: \(newItems)")
-        print("itemsFromColorHelper: newLevel: \(newLevel)")
+//        print("itemsFromColorHelper: newIndex: \(newIndex)")
+//        print("itemsFromColorHelper: newItems: \(newItems)")
+//        print("itemsFromColorHelper: newLevel: \(newLevel)")
         
         currentHighestIndex = newIndex
         items += newItems
@@ -404,8 +548,8 @@ struct ContentView: View {
 //        [.red, .green, .blue, .purple, .orange],
 //        sampleColors0,
 //        sampleColors1,
-//        sampleColors2,
-        sampleColors3,
+        sampleColors2,
+//        sampleColors3,
         Int(rectHeight))
     
     @State var isExpanded = false
@@ -425,9 +569,82 @@ struct ContentView: View {
             } // ForEach
         } // ZStack
         .animation(.default)
-        .offset(x: -200, y: -400)
+        .offset(x: -200, y: -300)
     }
 }
+
+
+let sampleColors0: [MyColor] = [
+    MyColor(color: .red),
+    MyColor(color: .blue),
+    MyColor(color: .green)
+]
+
+//let sampleColors1: [MyColor] = [
+//    MyColor(color: .red),
+//    MyColor(color: .blue, children: [
+//        MyColor(color: .black),
+//        MyColor(color: .brown)
+//    ]),
+//    MyColor(color: .green)
+//]
+
+//let sampleColors1: [MyColor] = [
+////    MyColor(color: .red),
+//    MyColor(color: .blue, children: [
+//        MyColor(color: .black),
+////        MyColor(color: .brown)
+//    ]),
+//    MyColor(color: .green)
+//]
+
+
+let sampleColors1: [MyColor] = [
+    MyColor(color: .red),
+    MyColor(color: .blue, children: [
+        MyColor(color: .black),
+        MyColor(color: .brown)
+    ]),
+    MyColor(color: .green),
+    MyColor(color: .yellow)
+]
+
+//let sampleColors1: [MyColor] = [
+//    MyColor(color: .red),
+//    MyColor(color: .blue, children: [
+//        MyColor(color: .black),
+////        MyColor(color: .brown)
+//    ]),
+//    MyColor(color: .green),
+//    MyColor(color: .yellow)
+//]
+
+let sampleColors2: [MyColor] = [
+    MyColor(color: .red),
+    MyColor(color: .blue, children: [
+        MyColor(color: .black),
+        MyColor(color: .brown, children: [
+            MyColor(color: .cyan),
+            MyColor(color: .purple),
+        ])
+    ]),
+    MyColor(color: .green)
+]
+
+let sampleColors3: [MyColor] = [
+    MyColor(color: .red),
+    MyColor(color: .blue, children: [
+        MyColor(color: .black),
+        MyColor(color: .brown, children: [
+            MyColor(color: .cyan),
+            MyColor(color: .purple, children: [
+                MyColor(color: .orange),
+                MyColor(color: .gray),
+            ]),
+        ])
+    ]),
+    MyColor(color: .green)
+]
 
 
 // CustomDG
@@ -455,96 +672,3 @@ struct ContentView: View {
 //    }
 //}
 
-
-struct RectItem: Identifiable, Equatable {
-    let id: Int
-    let color: Color
-    var location: CGPoint
-    var previousLocation: CGPoint
-    var children: [RectItem] = []
-    var zIndex: Int = 0
-    
-    // for converting items back into nested data
-    var parentId: Int? = nil
-    
-    init(id: Int, color: Color, location: CGPoint, children: [RectItem] = [], parentId: Int? = nil) {
-        self.id = id
-        self.color = color
-        self.location = location
-        self.previousLocation = location
-        self.children = children
-        self.parentId = parentId
-    }
-}
-
-extension Optional {
-    var isDefined: Bool {
-        self != nil
-    }
-}
-
-struct RectView2: View {
-    
-    var item: RectItem
-    @Binding var items: RectItems // all items
-    @Binding var current: Int?
-    
-    var body: some View {
-        Rectangle().fill(item.color)
-//            .border((current.map { $0 == item.id } ?? false) ? .gray : .clear,
-//                    width: 8)
-            .border((current.map { $0 == item.id } ?? false) ? .white : .clear,
-                    width: 16)
-         
-//            .overlay(Rect)
-            .frame(width: rectWidth, height: rectHeight)
-            .overlay(VStack {
-                Text("Id: \(item.id)")
-                Text("Parent?: \(item.parentId?.description ?? "None")")
-            }.scaleEffect(1.4)
-            )
-            .foregroundColor(.white)
-            .border(.orange)
-//            .position(item.location)
-//            .offset(CGSize(width: item.location.x,
-        
-            // while creating the items, can also create the indentation based not just on parentId.isDefined
-            // but also on how deep the level of nesting is
-//            .offset(CGSize(width: item.location.x + (item.parentId.isDefined ? 50 : 0),
-            .offset(CGSize(width: item.location.x,
-                           height: item.location.y))
-//            .zIndex(Double(item.zIndex))
-            .gesture(DragGesture()
-                        .onChanged({ value in
-                print("onChanged: \(item.id)")
-                // done in onDragged
-//                item.location = updatePosition(
-//                    translationHeight: value.translation.height,
-//                    location: item.previousLocation)
-                current = item.id
-                var item = item
-                item.zIndex = 9999
-                items = onDragged(item, // this dragged item
-                                  value.translation, // drag data
-                                  // ALL items
-                                  items)
-                
-                // ^^ now that we've updated the location of the item,
-                // we might need to also move the other items
-                
-                
-            })
-                        .onEnded({ _ in
-                print("onEnded: \(item.id)")
-                current = nil
-                var item = item
-                item.previousLocation = item.location
-                item.zIndex = 0 // set to zero when drag ended
-                let index = items.firstIndex { $0.id == item.id }!
-                items[index] = item
-                items = onDragEnded(item, items)
-                
-            })
-            ) // gesture
-    }
-}
